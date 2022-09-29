bdfb854a20cc914824a2821ec3fe718ef7582856
Grigoriy Legal <grigoriylegal@yandex.ru>
2022-09-29 20:59:14 +0300

    Задача все числа четные от одного до N

---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Number = 1;
+
+while(Number <= N)
+{
+    if(Number % 2 == 0)
+    
+        Console.Write(Number + ",");
+        Number ++;
+ 
+}
