5eda5be5bbbabba4ce06570deeb928c85c3a47c2
Grigoriy Legal <grigoriylegal@yandex.ru>
2022-09-29 19:37:31 +0300

    Первая задача готова

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = -9;
+int b = -3;
+
+int max = a;
+int min = b;
+
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+
+if (b > a)
+{
+    max = b;
+    min = a;
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+
+Console.Write("Минимальное число: ");
+Console.WriteLine(min);
