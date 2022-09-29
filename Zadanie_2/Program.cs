b30ae1288e79e7681aa478176bda65e7b8623722
Grigoriy Legal <grigoriylegal@yandex.ru>
2022-09-29 19:58:14 +0300

    Сделал второе здание

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
