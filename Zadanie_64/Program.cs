36a495c87cb3870ea2d3fc831358aee83d3c5b0f
unknown <grigoriylegal@yandex.ru>
2022-11-14 12:15:50 +0300

    Домашнее задание урок 9

---
+++ Zadanie_64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int EnterNumber (string message)
+{
+Console.Write (message);
+return Convert.ToInt32(Console.ReadLine()); 
+}
+
+void FindBetween (int numberN)
+{
+    if (numberN == 0) return;
+    else
+    Console.Write ($"{numberN}  ");
+    FindBetween (numberN-1);
+    
+
+}
+
+
+int number = EnterNumber ("Введите число N: ");
+Console.Write ($"N = {number} -> ");
+FindBetween (number);
---
+++ Zadanie_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int EnterNumber (string message)
+{
+Console.Write (message);
+return Convert.ToInt32(Console.ReadLine()); 
+}
+
+int FindSum (int numberM,  int numberN)
+{   int sum = default;
+    if (numberM <= numberN) return sum += numberM + FindSum (numberM+1, numberN);
+    else return sum;   
+}
+
+
+int numberM = EnterNumber ("Введите число M: ");
+int numberN = EnterNumber ("Введите число N: ");
+Console.Write ($"M = {numberM}; N = {numberN} ->  ");
+int answer = FindSum (numberM, numberN);
+Console.Write (answer);
